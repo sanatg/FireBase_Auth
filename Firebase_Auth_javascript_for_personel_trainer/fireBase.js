4c9521c635cc38a505716ada8686e6f03a02e0f5
sanatg <62789975+sanatg@users.noreply.github.com>
2020-11-24 21:43:54 +0530

    Add files via upload

---
+++ Firebase_Auth_javascript_for_personel_trainer/fireBase.js
@@ -0,0 +1,16 @@
+var app_fireBase = {}
+(function(){
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDVvDtr7PGq3M0NWuTlYyPUKREWfMMccz4",
+    authDomain: "fir-auth-73adb.firebaseapp.com",
+    databaseURL: "https://fir-auth-73adb.firebaseio.com",
+    projectId: "fir-auth-73adb",
+    storageBucket: "fir-auth-73adb.appspot.com",
+    messagingSenderId: "192456213504",
+    appId: "1:192456213504:web:cf61e5a7692ea5df2c04e5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ app_fireBase = firebase;
+})()
